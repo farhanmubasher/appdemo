ba1180ec2e929a446bf1e85b0078378621087c0a
farhanmubasher <farhanmubasher308@gmail.com>
2016-11-25 14:39:01 +0500

    Delete hi.R

--- hi.R
+++
@@ -1,8 +0,0 @@
-hi <- function(myname = ""){
-  if(myname == ""){
-    stop("Tell me your name!")
-  }
-  list(
-    message = paste("hello", myname, "! This is app test in Orbit software Solutions")
-  )
-}
